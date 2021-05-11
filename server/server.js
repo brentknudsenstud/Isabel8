d3f6b5cc7253cfaed4ad5c2fcbd6d1240e9329a0
Brent Knudsen <brent@Brents-MacBook-Pro.local>
2021-05-11 10:07:53 -0600

    put images above title for song clips on music page

--- server/server.js
+++ server/server.js
@@ -15,15 +15,15 @@ const express = require('express')
             const songclips = [
                 {name: 'Morning Comes', soundclip: '/assets/morning-comes.ogg', image: '/assets/morning-comes.jpg'},
                 {name: 'Bending Time', soundclip: '/assets/bending-time.ogg', image: '/assets/bending-time.jpg'},
-                {name: 'Crossover', soundclip: '/assets/crossover.ogg', image: '/assets/crossover.jpg' },
+                {name: 'Crossover', soundclip: '/assets/crossover.ogg', image: '/assets/crossover.png' },
                 {name: 'Pillars of Innocence', soundclip: '/assets/pillars-of-innocence.ogg', image: '/assets/pillars-of-innocence.jpg'},
-                {name: 'Phoenix Sunfire', soundclip: '/assets/phoenix-sunfire.ogg', image: '/assets/phoenix-sunfire.png'},
+                {name: 'Phoenix Sunfire', soundclip: '/assets/phoenix-sunfire.ogg', image: '/assets/phoenix-sunfire3.png'},
                 {name: 'Bottled Up Inside', soundclip: '/assets/bottled-up-inside.ogg', image: '/assets/bottled-up-inside.jpg'},
                 {name: 'Rising Meteor', soundclip: '/assets/rising-meteor.ogg', image: '/assets/rising-meteor.jpg'},
-                {name: 'Blue Rain', soundclip: '/assets/blue-rain.ogg', image: 'blue-rain'},
+                {name: 'Blue Rain', soundclip: '/assets/blue-rain.ogg', image: '/assets/blue-rain.jpg'},
                 {name: 'Expressionistic Reality', soundclip: '/assets/expressionistic-reality.ogg', image: '/assets/expressionistic-reality.jpg'},
                 {name: 'Soaking Up the Thunder', soundclip: '/assets/soaking-up-the-thunder.ogg', image: '/assets/soaking-up-the-thunder.jpg'},
-                {name: 'Chains of Liberty', soundclip: '/assets/chains-of-liberty.ogg', image: '/assets/chains-of-liberty.jpg'}
+                {name: 'Chains of Liberty', soundclip: '/assets/chains-of-liberty.ogg', image: '/assets/chains-of-liberty3.jpg'}
             ]
             response.send(songclips);
         })
--- src/components/SongClip.jsx
+++ src/components/SongClip.jsx
@@ -1,8 +1,9 @@
 export default function SongClip({ songclip }) {
     return (
         <div>
-            <h4>{songclip.name}</h4>
+            <p> </p>
             <img src={songclip.image} />
+            <h4>{songclip.name}</h4>
             <audio key={songclip.name} controls>
                 <source src={songclip.soundclip} type="audio/ogg" />
                         Your browser can not play audio files.
