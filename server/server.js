4606e04b08a21a8197b36984ac49b94331184f5a
Brent Knudsen <brent@Brents-MacBook-Pro.local>
2021-05-11 10:17:37 -0600

    put image first in songclips objects

--- server/server.js
+++ server/server.js
@@ -13,17 +13,17 @@ const express = require('express')
         app.get('/songclips', (request, response) => {
             
             const songclips = [
-                {name: 'Morning Comes', soundclip: '/assets/morning-comes.ogg', image: '/assets/morning-comes.jpg'},
-                {name: 'Bending Time', soundclip: '/assets/bending-time.ogg', image: '/assets/bending-time.jpg'},
-                {name: 'Crossover', soundclip: '/assets/crossover.ogg', image: '/assets/crossover.png' },
-                {name: 'Pillars of Innocence', soundclip: '/assets/pillars-of-innocence.ogg', image: '/assets/pillars-of-innocence.jpg'},
-                {name: 'Phoenix Sunfire', soundclip: '/assets/phoenix-sunfire.ogg', image: '/assets/phoenix-sunfire3.png'},
-                {name: 'Bottled Up Inside', soundclip: '/assets/bottled-up-inside.ogg', image: '/assets/bottled-up-inside.jpg'},
-                {name: 'Rising Meteor', soundclip: '/assets/rising-meteor.ogg', image: '/assets/rising-meteor.jpg'},
-                {name: 'Blue Rain', soundclip: '/assets/blue-rain.ogg', image: '/assets/blue-rain.jpg'},
-                {name: 'Expressionistic Reality', soundclip: '/assets/expressionistic-reality.ogg', image: '/assets/expressionistic-reality.jpg'},
-                {name: 'Soaking Up the Thunder', soundclip: '/assets/soaking-up-the-thunder.ogg', image: '/assets/soaking-up-the-thunder.jpg'},
-                {name: 'Chains of Liberty', soundclip: '/assets/chains-of-liberty.ogg', image: '/assets/chains-of-liberty3.jpg'}
+                {image: '/assets/morning-comes.jpg', name: 'Morning Comes', soundclip: '/assets/morning-comes.ogg'},
+                {image: '/assets/bending-time.jpg', name: 'Bending Time', soundclip: '/assets/bending-time.ogg'},
+                {image: '/assets/crossover.png', name: 'Crossover', soundclip: '/assets/crossover.ogg'},
+                {image: '/assets/pillars-of-innocence.jpg', name: 'Pillars of Innocence', soundclip: '/assets/pillars-of-innocence.ogg'},
+                {image: '/assets/phoenix-sunfire3.png', name: 'Phoenix Sunfire', soundclip: '/assets/phoenix-sunfire.ogg'},
+                {image: '/assets/bottled-up-inside.jpg', name: 'Bottled Up Inside', soundclip: '/assets/bottled-up-inside.ogg'},
+                {image: '/assets/rising-meteor.jpg', name: 'Rising Meteor', soundclip: '/assets/rising-meteor.ogg'},
+                {image: '/assets/blue-rain.jpg', name: 'Blue Rain', soundclip: '/assets/blue-rain.ogg'},
+                {image: '/assets/expressionistic-reality.jpg', name: 'Expressionistic Reality', soundclip: '/assets/expressionistic-reality.ogg'},
+                {image: '/assets/soaking-up-the-thunder.jpg', name: 'Soaking Up the Thunder', soundclip: '/assets/soaking-up-the-thunder.ogg'},
+                {image: '/assets/chains-of-liberty3.jpg', name: 'Chains of Liberty', soundclip: '/assets/chains-of-liberty.ogg'}
             ]
             response.send(songclips);
         })
